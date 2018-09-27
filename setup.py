67f3f5af303e7425f1d10dd717ecfb5ca98cc1f1
Asnim <asnim96@gmail.com>
2018-09-27 23:32:39 +0530

    Updated Description

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(name='nsehistorical',
       version='0.1',
-      description='The funniest joke in the world',
+      description='This package is used to get extract data from various NSE Source files',
       url='http://github.com/storborg/funniest',
       author='Asnim P Ansari',
       author_email='asnim96@gmail.com',
