f42da2964ae75e207cb25469b23a4514f8449aba
Asnim <asnim96@gmail.com>
2018-09-27 23:30:28 +0530

    Initial Commit

---
+++ nsedata/test.py
@@ -0,0 +1,4 @@
+
+
+def print_me():
+    print('Hello')
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='nsehistorical',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/storborg/funniest',
+      author='Asnim P Ansari',
+      author_email='asnim96@gmail.com',
+      license='MIT',
+      packages=['nsedata'],
+      zip_safe=False)
